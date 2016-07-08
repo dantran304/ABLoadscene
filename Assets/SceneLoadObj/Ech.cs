87e2dc0ee7d7624f565a8dd376a9e998ac18d145
Dan Tran <dantrandinhh@gmail.com>
2016-07-08 16:25:19 +0700

    upload prj

---
+++ Assets/SceneLoadObj/Ech.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ech : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log ("Hello, i'm Ech!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Assets/SceneLoadScene/SLSManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SLSManager : MonoBehaviour
+{
+
+	public Text text;
+
+	// Use this for initialization
+	void Start ()
+	{
+		text.text = "Hello, im Cube";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
